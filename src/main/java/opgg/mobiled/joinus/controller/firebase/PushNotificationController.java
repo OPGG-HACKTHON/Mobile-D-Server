7b5ade3127e875ea300d2b5e6d084739cdc502a9
jiwon_ryu <happyjw3910@gmail.com>
2021-09-01 15:12:55 +0900

    [FIX] add firebase (send notification by data)

--- src/main/java/opgg/mobiled/joinus/controller/firebase/PushNotificationController.java
+++ src/main/java/opgg/mobiled/joinus/controller/firebase/PushNotificationController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,28 @@ public class PushNotificationController {
     public PushNotificationController(PushNotificationService pushNotificationService) {
         this.pushNotificationService = pushNotificationService;
     }
-    
+
+    @PostMapping("/notification/topic")
+    public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
     @PostMapping("/notification/token")
     public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
         pushNotificationService.sendPushNotificationToToken(request);
-        System.out.println("princr");
         return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
     }
-    
+
+    @PostMapping("/notification/data")
+    public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @GetMapping("/notification")
+    public ResponseEntity sendSampleNotification() {
+        pushNotificationService.sendSamplePushNotification();
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
 }
--- src/main/java/opgg/mobiled/joinus/service/firebase/FCMService.java
+++ src/main/java/opgg/mobiled/joinus/service/firebase/FCMService.java
@@ -22,14 +22,29 @@ import com.google.gson.GsonBuilder;
 @Service
 public class FCMService {
     private Logger logger = LoggerFactory.getLogger(FCMService.class);
-    
-    
+
+
+    public void sendMessage(Map<String, String> data, PushNotificationRequest request)
+            throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageWithData(data, request);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message with data. Topic: " + request.getTopic() + ", " + response);
+    }
+
+    public void sendMessageWithoutData(PushNotificationRequest request)
+            throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageWithoutData(request);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message without data. Topic: " + request.getTopic() + ", " + response);
+    }
+
     public void sendMessageToToken(PushNotificationRequest request)
             throws InterruptedException, ExecutionException {
         Message message = getPreconfiguredMessageToToken(request);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String jsonOutput = gson.toJson(message);
         String response = sendAndGetResponse(message);
+        logger.info(request.getTitle()+ " / "+request.getMessage());
         logger.info("Sent message to token. Device token: " + request.getToken() + ", " + response+ " msg "+jsonOutput);
     }
     
