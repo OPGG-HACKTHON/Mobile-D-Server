8cd749ede551fd6c32dc3a308942919d00e07667
jiwon_ryu <happyjw3910@gmail.com>
2021-09-15 10:45:34 +0900

    [FIX] 평가 안받은 사람은 0으로 리턴, 없는 target_pk는 오류 메시지 리턴하기

--- src/main/java/opgg/mobiled/joinus/dao/UserDaoSqls.java
+++ src/main/java/opgg/mobiled/joinus/dao/UserDaoSqls.java
@@ -2,4 +2,5 @@ package opgg.mobiled.joinus.dao;
 
 public class UserDaoSqls {
     public static final String SELECT_USER_WITH_TOKEN = "SELECT * FROM user WHERE token = :token";
+    public static final String SELECT_USER_WITH_PK = "SELECT * FROM user WHERE pk = :pk";
 }
